158e64c89f05112987b2f363ad078c79063955ec
monitor81 <47352504+monitor81@users.noreply.github.com>
2021-08-02 17:55:06 +0300

    Update profile.php
    возможно ошибка

--- profile.php
+++ profile.php
@@ -1,4 +1,10 @@
 <?
 session_start();
-if (!empty($_SESSION['login'])) echo "Добро пожаловать, ".$_SESSION['login'];
+if (!empty($_SESSION['login'])) {
+	echo "Добро пожаловать, ".$_SESSION['login'];
+	include 'form_contact.php';
+
+
+
+}
 else echo "ВЫ не авторизовались";
