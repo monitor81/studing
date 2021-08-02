b5c308247432df682da3c1035867d70daca7819b
monitor81 <47352504+monitor81@users.noreply.github.com>
2021-08-02 17:18:32 +0300

    Update index.php
    Add form

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?
-echo "Hello";
+if (isset($_GET['login'])) {
+    echo "Привет ". $_GET['login'];
+}
+?>
+<form method= "GET">
+    <input type = "text" placeholder = "Введите ваша имя" name = 'login'><br>
+    <input type = "submit" value ="Ok">
+</form>
